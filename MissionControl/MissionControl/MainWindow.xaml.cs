a0e4ce0329270556fdb1617dad68c814966beb13
The Doctor <sciencedude100@yahoo.com>
2019-04-10 23:08:13 -0400

    Alpha V2.. live slider updates and HEX input

--- MissionControl/MissionControl/MainWindow.xaml.cs
+++ MissionControl/MissionControl/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -13,6 +15,8 @@ namespace MissionControl {
             BlueLabel.Text = "127";
             ColorChange();
             UpdateSerialSelector();
+            // Temp...
+            SerialPortSelector.Text = "COM6";
         }
 
         /* TODO:
@@ -26,16 +30,19 @@ namespace MissionControl {
             var color = (int)RedSlider.Value;
             RedLabel.Text = color.ToString();
             ColorChange();
+            SetLeds();
         }
 
         private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             GreenLabel.Text = GreenSlider.Value.ToString();
             ColorChange();
+            SetLeds();
         }
 
         private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             BlueLabel.Text = BlueSlider.Value.ToString();
             ColorChange();
+            SetLeds();
         }
 
         private void ColorChange() {
@@ -46,30 +53,10 @@ namespace MissionControl {
         }
 
         private void ChangeLed_Click(object sender, RoutedEventArgs e) {
-            // Set LEDs
-            uint red = (uint)RedSlider.Value;
-            uint green = (uint)GreenSlider.Value;
-            uint blue = (uint)BlueSlider.Value;
-            var LEDCount = 136;
-            // Map Lower/higher range to ASCII byte
-            uint lower = uint.Parse(lbox.Text);
-            uint higher = uint.Parse(hbox.Text);
-            // Create Serial Command as a String
-            string SerialCommand = red + "," + green + "," + blue + "," + lower + "," + higher + "\n";
-            
-            // TODO: Fix Int -> Byte
-
-            if (SerialPortSelector.Text == "")
-            {
+            if (SerialPortSelector.Text == "") {
                 MessageBox.Show("You need to select a serial port.");
-            }
-            else
-            {
-                SerialPort Serial = new SerialPort(SerialPortSelector.Text, 115200, Parity.None, 8, StopBits.One);
-                Serial.Handshake = Handshake.None;
-                Serial.Open();
-                Serial.Write(SerialCommand);
-                Serial.Close();
+            } else {
+                SetLeds();
             }
         }
         private void UpdateSerialSelector() {
@@ -82,13 +69,35 @@ namespace MissionControl {
             }
         }
 
+        private void SetLeds() {
+            // Set LEDs
+            uint red = (uint)RedSlider.Value;
+            uint green = (uint)GreenSlider.Value;
+            uint blue = (uint)BlueSlider.Value;
+            var LEDCount = 136;
+            // Map Lower/higher range to ASCII byte
+            uint lower = uint.Parse(lbox.Text);
+            uint higher = uint.Parse(hbox.Text);
+            // Create Serial Command as a String
+            string SerialCommand = red + "," + green + "," + blue + "," + lower + "," + higher + "\n";
+            if (SerialPortSelector.Text == "") {
+                //MessageBox.Show("You need to select a serial port.");
+            } else {
+                SerialPort Serial = new SerialPort(SerialPortSelector.Text, 115200, Parity.None, 8, StopBits.One);
+                Serial.Handshake = Handshake.None;
+                Serial.Open();
+                Serial.Write(SerialCommand);
+                Serial.Close();
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e) {
             UpdateSerialSelector();
         }
 
         private void RedLabel_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            if (RedLabel.Text == "") {
+            if (RedLabel.Text == "" || !RedLabel.Text.All(char.IsDigit) ) {
                 RedSlider.Value = 0.0;
             } else {
                 RedSlider.Value = Int32.Parse(RedLabel.Text);
@@ -97,7 +106,7 @@ namespace MissionControl {
         }
 
         private void GreenLabel_OnTextChanged(object sender, TextChangedEventArgs e) {
-            if (GreenLabel.Text == "")
+            if (GreenLabel.Text == "" || !GreenLabel.Text.All(char.IsDigit) )
             {
                 GreenSlider.Value = 0.0;
             }else
@@ -108,12 +117,52 @@ namespace MissionControl {
         }
 
         private void BlueLabel_OnTextChanged(object sender, TextChangedEventArgs e) {
-            if (BlueLabel.Text == "") {
+            if (BlueLabel.Text == "" || !BlueLabel.Text.All(char.IsDigit) ) {
                 BlueSlider.Value = 0.0;
             } else {
                 BlueSlider.Value = Int32.Parse(BlueLabel.Text);
             }
             ColorChange();
+        }
+
+        private void HexBox_OnTextChanged(object sender, TextChangedEventArgs e) {
+            // Handle HexColor input
+            var isHexColor = Regex.Match(HexBox.Text, "^#([A-Fa-f0-9]{6})$");
+            var isHexColorNoHash = Regex.Match(HexBox.Text, "^([A-Fa-f0-9]{6})$");
+            if (isHexColor.Success || isHexColorNoHash.Success) {
+                // It's a valid HEX color
+                var s = HexBox.Text.ToCharArray();
+                char[] redChar = new char[2], blueChar = new char[2], greenChar = new char[2];
+                int arrayOffset = 2;
+                if (s[0] == '#') {
+                    arrayOffset = 1;
+                }
+                redChar[0] = s[2 - arrayOffset];
+                redChar[1] = s[3 - arrayOffset];
+                greenChar[0] = s[4 - arrayOffset];
+                greenChar[1] = s[5 - arrayOffset];
+                blueChar[0] = s[6 - arrayOffset];
+                blueChar[1] = s[7 - arrayOffset];
+                string redString = new string(redChar);
+                string greenString = new string(greenChar);
+                string blueString = new string(blueChar);
+                uint red = Convert.ToUInt32(redString, 16);
+                uint green = uint.Parse(greenString, System.Globalization.NumberStyles.HexNumber);
+                uint blue = uint.Parse(blueString, System.Globalization.NumberStyles.HexNumber);
+                RedLabel.Text = red.ToString();
+                GreenLabel.Text = green.ToString();
+                BlueLabel.Text = blue.ToString();
+                RedSlider.Value = red;
+                GreenSlider.Value = green;
+                BlueSlider.Value = blue;
+                ColorChange();
+            }
+
+        }
+
+        private void LedPositionSlider_RangeValueChanged(object sender, RoutedEventArgs e) {
+            var LedsSelectedCount = LedPositionSlider.HigherValue - LedPositionSlider.LowerValue;
+            totalLabel.Text = LedsSelectedCount.ToString();
         }
     }
 }
